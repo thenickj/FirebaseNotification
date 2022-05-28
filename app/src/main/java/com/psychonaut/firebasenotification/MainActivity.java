8421f37c610569e407edbe7567d57bbead45dc94
thenickj <nickjohnson7113@gmail.com>
2022-05-27 20:58:21 -0400

    Initial commit

---
+++ app/src/main/java/com/psychonaut/firebasenotification/MainActivity.java
@@ -0,0 +1,22 @@
+package com.psychonaut.firebasenotification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
